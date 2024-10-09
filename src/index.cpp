ad41aaebb7a74e9011edd454e928800529cefc36
9p6p <1323212047@qq.com>
2024-10-09 10:24:32 +0800

    change format

--- src/index.cpp
+++ src/index.cpp
@@ -6,15 +6,18 @@
 #include <efanna2e/index.h>
 namespace efanna2e {
 Index::Index(const size_t dimension, const size_t n, Metric metric = L2)
-  : dimension_ (dimension), nd_(n), has_built(false) {
+    : dimension_(dimension), nd_(n), has_built(false) {
     switch (metric) {
-      case L2:distance_ = new DistanceL2();
-        break;
-      case INNER_PRODUCT: distance_ = new DistanceInnerProduct();
-        break;
-      default:distance_ = new DistanceL2();
-        break;
+        case L2:
+            distance_ = new DistanceL2();
+            break;
+        case INNER_PRODUCT:
+            distance_ = new DistanceInnerProduct();
+            break;
+        default:
+            distance_ = new DistanceL2();
+            break;
     }
 }
 Index::~Index() {}
-}
+}  // namespace efanna2e
--- tests/test_nsg_index.cpp
+++ tests/test_nsg_index.cpp
@@ -8,24 +8,24 @@
 
 void load_data(char* filename, float*& data, unsigned& num,
                unsigned& dim) {  // load data with sift10K pattern
-  std::ifstream in(filename, std::ios::binary);
-  if (!in.is_open()) {
-    std::cout << "open file error" << std::endl;
-    exit(-1);
-  }
-  in.read((char*)&dim, 4);
-  in.seekg(0, std::ios::end);
-  std::ios::pos_type ss = in.tellg();
-  size_t fsize = (size_t)ss;
-  num = (unsigned)(fsize / (dim + 1) / 4);
-  data = new float[(size_t)num * (size_t)dim];
+    std::ifstream in(filename, std::ios::binary);
+    if (!in.is_open()) {
+        std::cout << "open file error" << std::endl;
+        exit(-1);
+    }
+    in.read((char*)&dim, 4);
+    in.seekg(0, std::ios::end);
+    std::ios::pos_type ss = in.tellg();
+    size_t fsize = (size_t)ss;
+    num = (unsigned)(fsize / (dim + 1) / 4);
+    data = new float[(size_t)num * (size_t)dim];
 
-  in.seekg(0, std::ios::beg);
-  for (size_t i = 0; i < num; i++) {
-    in.seekg(4, std::ios::cur);
-    in.read((char*)(data + i * dim), dim * 4);
-  }
-  in.close();
+    in.seekg(0, std::ios::beg);
+    for (size_t i = 0; i < num; i++) {
+        in.seekg(4, std::ios::cur);
+        in.read((char*)(data + i * dim), dim * 4);
+    }
+    in.close();
 }
 
 template <class T>
@@ -50,39 +50,39 @@ T* read_bin(const char* filename, uint32_t& npts, uint32_t& dim) {
 }
 
 int main(int argc, char** argv) {
-  if (argc != 7) {
-    std::cout << argv[0] << " data_file nn_graph_path L R C save_graph_file"
-              << std::endl;
-    exit(-1);
-  }
-  //float* data_load = NULL;
-  unsigned points_num, dim;
-  //load_data(argv[1], data_load, points_num, dim);
-  float* data_load = read_bin<float>(argv[1], points_num, dim);
+    if (argc != 7) {
+        std::cout << argv[0] << " data_file nn_graph_path L R C save_graph_file"
+                  << std::endl;
+        exit(-1);
+    }
+    // float* data_load = NULL;
+    unsigned points_num, dim;
+    // load_data(argv[1], data_load, points_num, dim);
+    float* data_load = read_bin<float>(argv[1], points_num, dim);
 
-  std::string nn_graph_path(argv[2]);
-  unsigned L = (unsigned)atoi(argv[3]);
-  unsigned R = (unsigned)atoi(argv[4]);
-  unsigned C = (unsigned)atoi(argv[5]);
+    std::string nn_graph_path(argv[2]);
+    unsigned L = (unsigned)atoi(argv[3]);
+    unsigned R = (unsigned)atoi(argv[4]);
+    unsigned C = (unsigned)atoi(argv[5]);
 
-  // data_load = efanna2e::data_align(data_load, points_num, dim);//one must
-  // align the data before build
-  efanna2e::IndexNSG index(dim, points_num, efanna2e::INNER_PRODUCT, nullptr);
+    // data_load = efanna2e::data_align(data_load, points_num, dim);//one must
+    // align the data before build
+    efanna2e::IndexNSG index(dim, points_num, efanna2e::INNER_PRODUCT, nullptr);
 
-  auto s = std::chrono::high_resolution_clock::now();
-  efanna2e::Parameters paras;
-  paras.Set<unsigned>("L", L);
-  paras.Set<unsigned>("R", R);
-  paras.Set<unsigned>("C", C);
-  paras.Set<std::string>("nn_graph_path", nn_graph_path);
+    auto s = std::chrono::high_resolution_clock::now();
+    efanna2e::Parameters paras;
+    paras.Set<unsigned>("L", L);
+    paras.Set<unsigned>("R", R);
+    paras.Set<unsigned>("C", C);
+    paras.Set<std::string>("nn_graph_path", nn_graph_path);
 
-  omp_set_num_threads(omp_get_num_procs());
-  index.Build(points_num, data_load, paras);
-  auto e = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> diff = e - s;
+    omp_set_num_threads(omp_get_num_procs());
+    index.Build(points_num, data_load, paras);
+    auto e = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diff = e - s;
 
-  std::cout << "indexing time: " << diff.count() << "\n";
-  index.Save(argv[6]);
+    std::cout << "indexing time: " << diff.count() << "\n";
+    index.Save(argv[6]);
 
-  return 0;
+    return 0;
 }
