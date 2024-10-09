0a5e91f2fcb82b0725a9386706e944d6abac0e99
9p6p <1323212047@qq.com>
2024-10-09 10:12:27 +0800

    add ip

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ save_prefix=/root/indices/${dataset}
 
 cd build
 
-./tests/test_nsg_index ${prefix}/base.10M.fbin ${save_prefix}/${dataset}.knn 500 64 500 ${save_prefix}/${dataset}.nsg
+./tests/test_nsg_index ${prefix}/base.10M.fbin ${save_prefix}/${dataset}.knn 500 64 500 ${save_prefix}/${dataset}.ipnsg
